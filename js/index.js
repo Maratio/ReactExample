e7e73c50066fe46c1b5c91b6c74c40d0cc8f940e
Marat Kamalutdinov <marat.kmr@yandex.ru>
2023-08-03 23:45:46 +0300

    SecondCommit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
+<<<<<<< HEAD
 
 ReactDOM.render(<h1>Privet</h1>, document.getElementById("app"))
+=======
+// ReactDOM.render(React.createElement("input",{
+//     placeholder: "Help text",
+//     onClick: () => console.log("Clicked"),
+//     onMouseEnter: () => console.log("Mouse Over") 
+// }), document.getElementById("app"))
+
+// ReactDOM.render(<input placeholder = "Help text"/>, document.getElementById("app"))
+
+// const inputclick = () => console.log("Clicked")
+// const mouseOver = () => console.log("Mouse Over")
+// const el = <input placeholder = "Help text" onClick={inputclick} onMouseEnter={mouseOver}/>
+// const app = document.getElementById("app")
+// ReactDOM.render(el, app )
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = () => console.log("Mouse Over")
+
+const helpText = "Help text"
+const el = 
+(<div>
+    <h1>{helpText}</h1>
+    <input placeholder = {helpText} onClick = {inputClick} onMouseEnter={mouseOver}/>
+<p>
+    {helpText == "Help text" ? "Yes" : "No"}
+</p>
+</div>)
+const app = document.getElementById("app")
+ReactDOM.render(el, app )
+>>>>>>> 84daa7b (SecondCommit)
